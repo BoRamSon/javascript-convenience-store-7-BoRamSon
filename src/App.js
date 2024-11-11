a37f64dedc1dbc03f6bedd539c34f78ec6833654
BoRamSon <onbrson1@gmail.com>
2024-11-11 23:48:26 +0900

    feat(StoreController): 연동적인 동작을 할 수 있도록 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import StoreController from "./controller/StoreController.js";
+
 class App {
-  async run() {}
+  constructor() {
+    this.storeController = new StoreController();
+  }
+  async run() {
+    this.storeController.startStore();
+  }
 }
 
 export default App;
--- src/controller/StoreController.js
+++ src/controller/StoreController.js
@@ -1,17 +1,22 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import InputProductsHandler from "../modules/InputProductsHandler.js";
 import PrintGreetingAndInventory from "../modules/PrintGreatingAndInventory.js";
+import PriceAndPromotionHandler from "../modules/PriceAndPromotionHandler.js";
+
 
 class StoreController {
   constructor() {
     this.printGreetingAndInventory = new PrintGreetingAndInventory();
     this.inputProductsHandler = new InputProductsHandler();
+    this.priceAndPromotionHandler = new PriceAndPromotionHandler();
   }
 
-  startStore() {
+  async startStore() {
     this.printGreetingAndInventory.run();
     MissionUtils.Console.print("");
-    this.inputProductsHandler.receiveInputDuringError();
+    const validatedInput = await this.inputProductsHandler.receiveInputDuringError();
+    // await this.priceAndPromotionHandler.run(validatedInput);
+    // ... 제출로 인한 구현 중지
   }
 }
 
